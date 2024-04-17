79f86982575ecc8a83a5983b8e439287b3a85277
Majid <Majid@DESKTOP-832683M>
2024-04-17 21:15:14 +0530

    New Class Added With Methods

--- src/apr12/Demo.java
+++ src/apr12/Demo.java
@@ -1,5 +1,22 @@
 package apr12;
 
 public class Demo {
-
+public void add()
+{
+	int a=34,b=56,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=300,b=5,c;
+	c=a/b;
+	System.out.println(c);
+}
+public void sub()
+{
+	int a=349,b=156,c;
+	c=a-b;
+	System.out.println(c);
+}
 }
