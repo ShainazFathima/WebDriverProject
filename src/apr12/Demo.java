7fd449fbeb941e422dca8d3dbcfe99f7c8e10ee5
Majid <Majid@DESKTOP-832683M>
2024-04-17 21:18:52 +0530

    Method values modified

--- src/apr12/Demo.java
+++ src/apr12/Demo.java
@@ -3,19 +3,19 @@ package apr12;
 public class Demo {
 public void add()
 {
-	int a=34,b=56,c;
+	int a=341,b=566,c;
 	c=a+b;
 	System.out.println(c);
 }
 public void div()
 {
-	int a=300,b=5,c;
+	int a=305,b=5,c;
 	c=a/b;
 	System.out.println(c);
 }
 public void sub()
 {
-	int a=349,b=156,c;
+	int a=348,b=156,c;
 	c=a-b;
 	System.out.println(c);
 }
